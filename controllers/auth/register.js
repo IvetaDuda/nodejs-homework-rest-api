8ccb7be177e0fa25582f6f30feb0402f98c2e256
IvetaDuda <92565279+IvetaDuda@users.noreply.github.com>
2022-10-27 22:12:32 +0300

    Added user email verification after registration

--- controllers/auth/register.js
+++ controllers/auth/register.js
@@ -1,10 +1,11 @@
 const bcrypt = require('bcryptjs');
+const { nanoid } = require('nanoid');
 
 const gravatar = require('gravatar');
 
 const { User } = require('../../models');
 
-const { RequestError } = require('../../helpers');
+const { RequestError, sendEmail, createVerifyEmail } = require('../../helpers');
 
 const register = async (req, res) => {
   const { password, email, subscription } = req.body;
@@ -17,16 +18,25 @@ const register = async (req, res) => {
 
   const avatarURL = gravatar.url(email);
 
+  const verificationToken = nanoid();
+
   const result = await User.create({
     password: hashPassword,
     email,
     subscription,
     avatarURL,
+    verificationToken,
   });
+
+  const mail = createVerifyEmail(email, verificationToken);
+
+  await sendEmail(mail);
+
   res.status(201).json({
     user: {
       email: result.email,
       subscription: result.subscription,
+      verificationToken: result.verificationToken,
     },
   });
 };
